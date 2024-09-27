483f2dd804b2243ea84cb5a587e04c53f264910d
Mari Gomes <152400681+Marigomes99@users.noreply.github.com>
2024-09-26 23:06:10 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const path = require('path');
+const compression = require('compression'); // Importando o middleware de compressão
 const pageController = require('./controllers/pageController');
 
 dotenv.config();
@@ -9,6 +10,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(compression()); // Usando o middleware de compressão
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
